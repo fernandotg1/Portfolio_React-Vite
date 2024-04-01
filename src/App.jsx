a7541017c21beae1c16757a3886e21730b90e4f1
fernandotg1 <fernandotg1@gmail.com>
2024-04-01 10:28:52 -0300

    Foto adicionada a apresentação

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Footer from './components/section/Footer'
 import Navbar from './components/section/Navbar'
 import Presentation from './components/section/Presentation'
 import Project from './components/section/Project'
-import { useState } from 'react'
 import './App.css'
 
 
--- src/components/section/Presentation.jsx
+++ src/components/section/Presentation.jsx
@@ -5,19 +5,20 @@ function Presentation() {
     return (
         <div className={styles.presentation}>
             <div>
-            <h1>
-                Olá, eu sou o Fernando, <br/>
-                Desenvolvedor Front-End
-            </h1>
-            <p> 
-                Sou formado em Sistemas de Informação e venho acompanhando a tecnologia
-                ao longo dos anos com dedicação em áreas de suporte e infraestrutura. Atualmente
-                me dedico aos estudos para a migração de área com o objetivo de adentrar nesse
-                universo fascinante do desenvolvimento, e assim colocar em prática o aprendizado.
-            </p>
-            <br/>
-            <a href='https://github.com/fernandotg1'><button>Acessar Portfólio</button></a>
+                <h1>
+                    Olá, eu sou o Fernando, <br />
+                    Desenvolvedor Front-End
+                </h1>
+                <p>
+                    Sou formado em Sistemas de Informação e venho acompanhando a tecnologia
+                    ao longo dos anos com dedicação em áreas de suporte e infraestrutura. Atualmente
+                    me dedico aos estudos para a migração de área com o objetivo de adentrar nesse
+                    universo fascinante do desenvolvimento, e assim colocar em prática o aprendizado.
+                </p>
+                <br />
+                <a href='https://github.com/fernandotg1'><button>Acessar Portfólio</button></a>
             </div>
+            <div className={styles.foto}><img src={foto} /></div>
         </div>
     )
 }
