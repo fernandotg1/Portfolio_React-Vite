0d301a7d08c38112fad54109745ab36e1dac60ce
fernandotg1 <fernandotg1@gmail.com>
2024-04-01 14:28:05 -0300

    Ajustes navbar e footer

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -4,8 +4,8 @@ import styles from './Footer.module.css'
 function Footer() {
     return (
         <div className={styles.footer} id='contact'>
-            <a href="https://github.com/fernandotg1"><FaGithub size='30px' /></a>
-            <a href="https://www.linkedin.com/in/fernando-tabor-gon%C3%A7alves-72777466"><FaLinkedin size='30px' /></a>
+            <a href="https://github.com/fernandotg1"><FaGithub size='40px' /></a>
+            <a href="https://www.linkedin.com/in/fernando-tabor-gon%C3%A7alves-72777466"><FaLinkedin size='40px' /></a>
             <p>Copyright ©2024 All rights reserved</p>
         </div>
     )
